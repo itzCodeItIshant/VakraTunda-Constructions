aeef2a036bf2e5868114324c062e39d5e625cb3d
codeitishant <spchoudhary64358@gmail.com>
2022-04-27 13:52:27 +0530

    sode

--- QuateCalculator.py
+++ QuateCalculator.py
@@ -2,9 +2,3 @@
 # ----------------------------------------------------------------
 
 # values for calculation
-myDict = {
-	"Name" : "Pranav",
-	"Age" : 45
-}
-a = input("Enter a string : ")
-print("Your answer : ", myDict.get(a))
